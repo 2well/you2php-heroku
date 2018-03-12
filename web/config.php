7cf4ae4a77420754cf7b4b7201644978de86d9ca
ajhwell <ajhwell@qq.com>
2018-03-12 02:29:22 +0000

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
+define('APIKEY', 'AIzaSyDt_kaevHSSklfAFZOorWfjpaduXYBAPew');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('SITE_NAME', '建华');//网站名字
+define('TITLENAME', '建华');//网站名字
+define('EN2DEKEY', 'HSSklfAFZOorWfjpaduXYBAPew');//一个随机字符串，加解密KEY
+define('EMAIL', 'j7i99t@twocowmail.net');//邮箱
 ?>
