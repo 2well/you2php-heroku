17ea727842efc09f4687b12be9d4f77a7c39e841
ajhwell <ajhwell@qq.com>
2018-03-12 02:35:30 +0000

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDt_kaevHSSklfAFZOorWfjpaduXYBAPew');//这里改成您的api key
+define('APIKEY', 'AIzaSyDt_kaev');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
 define('SITE_NAME', '建华');//网站名字
 define('TITLENAME', '建华');//网站名字
